41e7beb34a41560b0cd6c20d7f47b2f3e68cec34
keithz <keithzhf@gmail.com>
2020-05-15 14:50:22 +0800

    update

--- packages/script-tool/index.ts
+++ packages/script-tool/index.ts
@@ -35,13 +35,22 @@ interface GraphinData {
 const { readFileSync, readdirSync } = fs;
 
 const rootDir = '/Users/keith/project/qianmi/invite-card/src';
+const componentsRootDir =
+  '/Users/keith/project/qianmi/invite-card/src/components';
 // const rootDir = '/Users/keith/project/qianmi/invite-card/src/qmkits'
 
 // const fileName = rootDir + '/app.wpy'
 // const fileContent = readFileSync(fileName)
 // const node = ts.createSourceFile('x.ts', fileContent.toString(), ts.ScriptTarget.ES2015)
 
-const commonLib = ['wepy', 'wepy-async-function', 'dayjs', 'immutable', 'js-base64', 'events'];
+const commonLib = [
+  'wepy',
+  'wepy-async-function',
+  'dayjs',
+  'immutable',
+  'js-base64',
+  'events',
+];
 
 let graphinData: GraphinData = {
   nodes: commonLib.map((v) => ({
@@ -58,13 +67,17 @@ let graphinData: GraphinData = {
   edges: [],
 };
 
-const transform = (rootDir: string) => {
-  console.log('rootDir...', rootDir);
+let componentsGraphinData: { nodes: Array<GraphinNode> } = {
+  nodes: [],
+};
+
+const transformComponentsNode = (rootDir: string) => {
+  console.log('transformComponentsNode rootDir...', rootDir);
   const dir = readdirSync(rootDir, { withFileTypes: true });
   for (const item of dir) {
-    console.log('dir...', item);
+    // console.log('dir...', item);
     if (item.isFile()) {
-      console.log('file...', item.name);
+      // console.log('file...', item.name);
       const wpyFileIndex = item.name.indexOf('.wpy');
       const tsFileIndex = item.name.indexOf('.ts');
       const jsFileIndex = item.name.indexOf('.js');
@@ -90,13 +103,73 @@ const transform = (rootDir: string) => {
           suffixIndex = item.name.length;
         }
 
-        console.log('suffixIndex...', suffixIndex);
+        // console.log('suffixIndex...', suffixIndex);
         const name = item.name.substring(0, suffixIndex);
-        console.log('name...', name);
+        // console.log('name...', name);
         const prefix = rootDir.substr(rootDir.indexOf('/src') + 5);
         const nodeId = prefix ? prefix + '/' + name : name;
-        console.log('prefix...', prefix);
-        console.log('nodeId...', nodeId);
+        // console.log('prefix...', prefix);
+        // console.log('nodeId...', nodeId);
+        componentsGraphinData.nodes.push({
+          id: nodeId,
+          shape: 'CircleNode',
+          label: nodeId,
+          data: {
+            id: nodeId,
+            label: nodeId,
+            type: 'company',
+          },
+          style: {},
+        });
+      }
+    }
+
+    if (item.isDirectory()) {
+      // console.log('dir name...', item.name)
+      transformComponentsNode(rootDir + '/' + item.name);
+    }
+  }
+};
+
+const transform = (rootDir: string) => {
+  // console.log('rootDir...', rootDir);
+  const dir = readdirSync(rootDir, { withFileTypes: true });
+  for (const item of dir) {
+    // console.log('dir...', item);
+    if (item.isFile()) {
+      // console.log('file...', item.name);
+      const wpyFileIndex = item.name.indexOf('.wpy');
+      const tsFileIndex = item.name.indexOf('.ts');
+      const jsFileIndex = item.name.indexOf('.js');
+      const wxsFileIndex = item.name.indexOf('.wxs');
+
+      if (
+        wpyFileIndex != -1 ||
+        tsFileIndex != -1 ||
+        wxsFileIndex != -1 ||
+        jsFileIndex != -1
+      ) {
+        let suffixIndex = 0;
+        if (wpyFileIndex != -1) {
+          suffixIndex = wpyFileIndex;
+        }
+        if (tsFileIndex != -1) {
+          suffixIndex = tsFileIndex;
+        }
+        if (jsFileIndex != -1) {
+          suffixIndex = jsFileIndex;
+        }
+        if (wxsFileIndex != -1) {
+          suffixIndex = item.name.length;
+        }
+
+        // console.log('suffixIndex...', suffixIndex);
+        const name = item.name.substring(0, suffixIndex);
+        // console.log('name...', name);
+        const prefix = rootDir.substr(rootDir.indexOf('/src') + 5);
+        const nodeId = prefix ? prefix + '/' + name : name;
+        // console.log('prefix...', prefix);
+        // console.log('nodeId...', nodeId);
         graphinData.nodes.push({
           id: nodeId,
           shape: 'CircleNode',
@@ -147,13 +220,59 @@ const transform = (rootDir: string) => {
   }
 };
 
+let nodeTraceTree: any = {};
+
+const nodeTrace = (
+  nodeTraceTree: any,
+  rootNodeId: string,
+  subNodeId?: string
+) => {
+  const currentNodeId = subNodeId ? subNodeId : rootNodeId;
+  const nodeList = graphinData.edges
+    .filter((v) => v.target == currentNodeId)
+    .map((v) => ({ id: v.source }));
+
+  if (nodeList.length > 0) {
+    nodeTraceTree[rootNodeId] = nodeTraceTree[rootNodeId]
+      ? nodeTraceTree[rootNodeId].concat(nodeList)
+      : nodeList;
+
+    console.log('nodeList...', nodeList);
+    nodeList.forEach((v) => {
+      return nodeTrace(nodeTraceTree, rootNodeId, v.id);
+    });
+  }
+};
+
+transformComponentsNode(componentsRootDir);
 transform(rootDir);
+for (let node of componentsGraphinData.nodes) {
+  console.log('node...', node);
+  nodeTrace(nodeTraceTree, node.id);
+}
 
-const customePath =
-  '/Users/keith/project/mine/local-dependency-analyse/packages/client/src/components/Analyse/data.json';
+console.log('nodeTraceTree...', JSON.stringify(nodeTraceTree));
 
-// path.resolve(__dirname, "data.json")
-fs.writeFileSync(customePath, JSON.stringify(graphinData));
+// const customePath =
+//   '/Users/keith/project/mine/local-dependency-analyse/packages/client/src/components/Analyse/data.json';
+// fs.writeFileSync(customePath, JSON.stringify(nodeTraceTree));
 
-// console.log('graphin data...', graphinData)
+const dataPath = path.resolve(__dirname, 'data.json');
+fs.writeFileSync(dataPath, JSON.stringify(nodeTraceTree));
+
+const analysisArray = Object.keys(nodeTraceTree)
+  .map((k) => {
+    const pageReferenceLength = nodeTraceTree[k].filter((v: any) =>
+      v.id.startsWith('pages')
+    ).length;
+    return {
+      id: k,
+      pageReferenceLength,
+    };
+  })
+  .sort((a, b) => a.pageReferenceLength - b.pageReferenceLength);
+const analysisPath = path.resolve(__dirname, 'analysis.json');
+fs.writeFileSync(analysisPath, JSON.stringify(analysisArray));
+
+// console.log('graphin data...', graphinData.nodes.length)
 // console.log('graphin data node length...', graphinData.nodes.length)
